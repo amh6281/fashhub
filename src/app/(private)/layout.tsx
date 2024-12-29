1479361551b02c846b311b042352b56b6d3bb7f8
amh6281 <amh6281@naver.com>
2024-12-29 16:32:11 +0900

    Feat: Private layout 추가

---
+++ src/app/(private)/layout.tsx
@@ -0,0 +1,19 @@
+interface LayoutProps {
+  children: React.ReactNode;
+  modal: React.ReactNode;
+}
+
+const Layout = ({ children, modal }: LayoutProps) => {
+  return (
+    <div className='mx-auto flex max-w-screen-xl justify-between'>
+      <div className='px-8'>leftbar</div>
+      <div className='min-w-[600px] flex-1 border-x-[1px] border-gray-400'>
+        {children}
+        {modal}
+      </div>
+      <div className='ml-8 flex flex-1'>rightbar</div>
+    </div>
+  );
+};
+
+export default Layout;
