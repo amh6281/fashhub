a52d4308ee0f6c0b62c8d9c19e9b34cd606bdab5
amh6281 <amh6281@naver.com>
2024-12-30 13:43:01 +0900

    Feat: Recommendations component 추가

--- src/components/Home/index.ts
+++ src/components/Home/index.ts
@@ -1,2 +1,3 @@
 export { default as SearchBar } from './SearchBar';
 export { default as TrendTags } from './TrendTags';
+export { default as Recommendations } from './Recommendations';
--- src/components/Layout/RightBar.tsx
+++ src/components/Layout/RightBar.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { SearchBar, TrendTags } from '../Home';
+import { Recommendations, SearchBar, TrendTags } from '../Home';
 
 const RightBar = () => {
   return (
     <div className='sticky top-0 flex h-max w-full flex-col gap-4 pt-4'>
       <SearchBar />
       <TrendTags />
+      <Recommendations />
     </div>
   );
 };
