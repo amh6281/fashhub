75a8b9efb9926be743846b278c25576406963950
amh6281 <amh6281@naver.com>
2025-01-16 19:24:59 +0900

    Feat: handleError 추가

---
+++ src/utils/handleError.ts
@@ -0,0 +1,7 @@
+export const showMessage = (message: string | null) => {
+  if (message === 'no_email') return '이메일을 입력하세요.';
+  if (message === 'no_password') return '비밀번호를 입력하세요.';
+  if (message === 'no_name') return '성명을 입력하세요.';
+  if (message === 'no_nickname') return '사용자 이름을 입력하세요.';
+  return '';
+};
