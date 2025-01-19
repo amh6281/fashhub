7b6a617a936a0f44b39f83543cba6fbbb252d15d
amh6281 <amh6281@naver.com>
2025-01-19 12:57:48 +0900

    Feat: home 페이지 react-query를 이요한 데이터 패칭

--- src/app/(private)/home/page.tsx
+++ src/app/(private)/home/page.tsx
@@ -1,12 +1,42 @@
 import { Post } from '@/components/common/post';
 import { PostForm, Tab } from '@/components/Home';
+import { baseUrl } from '@/config';
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
 
-const Home = () => {
+const getRecommendedPosts = async () => {
+  const res = await fetch(`${baseUrl}/api/postRecommends`, {
+    next: {
+      tags: ['posts', 'recommend'],
+    },
+    cache: 'no-store',
+  });
+
+  if (!res.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return res.json();
+};
+
+const Home = async () => {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    // queryKey가 posts, recommend이면 getRecommendedPosts 함수를 호출
+    queryKey: ['posts', 'recommend'],
+    queryFn: getRecommendedPosts,
+  });
+  const dehydratedState = dehydrate(queryClient);
+
   return (
     <div className=''>
-      <Tab />
-      <PostForm />
-      <Post />
+      <HydrationBoundary state={dehydratedState}>
+        <Tab />
+        <PostForm />
+        <Post />
+      </HydrationBoundary>
     </div>
   );
 };
