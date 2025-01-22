30612905012bd600a2daf41705a5df3b5a965c0d
amh6281 <amh6281@naver.com>
2025-01-22 18:26:37 +0900

    Feat: FollowingPosts 컴포넌트 추가

---
+++ src/components/common/post/FollowingPosts.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { getFollowingPosts } from '@/lib/api';
+import { useQuery } from '@tanstack/react-query';
+import Post from './Post';
+import { PostType } from '@/types/Post';
+
+const FollowingPosts = () => {
+  const { data } = useQuery<PostType[]>({
+    queryKey: ['posts', 'followings'],
+    queryFn: getFollowingPosts,
+    staleTime: 60 * 1000, // fresh에서 stale로 전환되는 시간 (1분)
+    gcTime: 5 * 60 * 1000, // react-query 캐시에 저장된 데이터가 제거되는 시간 (5분)
+  });
+
+  return data?.map((post) => <Post key={post.postId} post={post} />);
+};
+
+export default FollowingPosts;
