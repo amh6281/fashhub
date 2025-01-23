27edc04d33593c81def10a8d40e722865c5f6be7
amh6281 <amh6281@naver.com>
2025-01-23 19:24:50 +0900

    Feat: SearchInput next/form 적용

--- src/components/common/SearchInput.tsx
+++ src/components/common/SearchInput.tsx
@@ -1,19 +1,32 @@
 import { MagnifyingGlass } from '@phosphor-icons/react/dist/ssr';
+import Form from 'next/form';
 
 interface SearchInputProps {
-  query?: string;
+  searchQuery: {
+    query?: string;
+    filter?: string;
+    pf?: string;
+  };
 }
 
-const SearchInput = ({ query }: SearchInputProps) => {
+const SearchInput = ({ searchQuery }: SearchInputProps) => {
   return (
-    <div className='flex items-center gap-4 rounded-full bg-gray-100 px-4 py-2'>
+    <Form
+      action='/search'
+      className='flex items-center gap-4 rounded-full bg-gray-100 px-4 py-2'
+    >
       <MagnifyingGlass size={16} />
       <input
-        type='text'
+        type='search'
+        name='query'
+        defaultValue={searchQuery.query}
         placeholder='검색'
         className='bg-transparent outline-none'
       />
-    </div>
+      {/* 숨겨진 입력 필드도 함께 form으로 전송 */}
+      <input type='hidden' name='pf' defaultValue={searchQuery?.pf} />
+      <input type='hidden' name='filter' defaultValue={searchQuery?.filter} />
+    </Form>
   );
 };
 
