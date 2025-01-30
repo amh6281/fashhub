ebd6e09ef2acf19307073bc2df383d01f033dad5
amh6281 <amh6281@naver.com>
2025-01-30 09:07:45 +0900

    Feat: SearchFilter query 추가

--- src/components/common/SearchFilter.tsx
+++ src/components/common/SearchFilter.tsx
@@ -1,19 +1,35 @@
+'use client';
+
 import React from 'react';
 import { Label } from '@/components/ui/label';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 const SearchFilter = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const handleChange = (value: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+    if (value === 'follow') {
+      params.set('pf', 'on');
+    } else {
+      params.delete('pf');
+    }
+    router.replace(`/search?${params.toString()}`);
+  };
+
   return (
     <div className='rounded-lg border-[1px] border-gray-300 p-4'>
       <h5 className='mb-4 text-lg font-bold'>검색 필터</h5>
-      <RadioGroup defaultValue='option-one'>
+      <RadioGroup defaultValue='option-one' onValueChange={handleChange}>
         <div className='flex items-center space-x-2'>
-          <RadioGroupItem value='option-one' id='option-one' />
-          <Label htmlFor='option-one'>모든 사용자</Label>
+          <RadioGroupItem value='all' id='all' />
+          <Label htmlFor='all'>모든 사용자</Label>
         </div>
         <div className='flex items-center space-x-2'>
-          <RadioGroupItem value='option-two' id='option-two' />
-          <Label htmlFor='option-two'>내가 팔로우하는 사람들</Label>
+          <RadioGroupItem value='follow' id='follow' />
+          <Label htmlFor='follow'>내가 팔로우하는 사람들</Label>
         </div>
       </RadioGroup>
     </div>
