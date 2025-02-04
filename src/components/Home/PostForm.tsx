730cceb37e25fa2edf316038d78ec3d434d265be
amh6281 <amh6281@naver.com>
2025-02-04 19:44:16 +0900

    Feat: 게시글 생성 기능 추가

--- src/components/Home/PostForm.tsx
+++ src/components/Home/PostForm.tsx
@@ -1,16 +1,61 @@
 'use client';
 
-import { ImageSquare, UserCircle } from '@phosphor-icons/react/dist/ssr';
+import {
+  ImageSquare,
+  UserCircle,
+  XCircle,
+} from '@phosphor-icons/react/dist/ssr';
 import { Button } from '../ui/button';
 import { useRef, useState } from 'react';
 import { Textarea } from '../ui/textarea';
+import Image from 'next/image';
+import { createPost } from '@/lib/api';
 
 const PostForm = () => {
   const imageRef = useRef<HTMLInputElement>(null);
   const [content, setContent] = useState('');
+  const [preview, setPreview] = useState<
+    Array<{ dataUrl: string; file: File } | null>
+  >([]);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append('content', content);
+    preview.forEach((p) => {
+      if (p) {
+        formData.append('images', p.file);
+      }
+    });
+
+    createPost(formData);
+  };
+
+  const handleUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    if (!e.target.files) return;
+    Array.from(e.target.files).forEach((file, index) => {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreview((prevPreview) => {
+          const prev = [...prevPreview];
+          prev[index] = {
+            dataUrl: reader.result as string,
+            file,
+          };
+          return prev;
+        });
+      };
+      reader.readAsDataURL(file);
+    });
+  };
 
   return (
-    <form className='flex gap-4 border-b border-gray-300 p-4'>
+    <form
+      className='flex gap-4 border-b border-gray-300 p-4'
+      onSubmit={handleSubmit}
+    >
       {/* avatar */}
       <div className='relative flex h-9 w-9 items-center justify-center overflow-hidden rounded-full'>
         <UserCircle size={36} className='text-gray-500' />
@@ -23,12 +68,49 @@ const PostForm = () => {
           onChange={(e) => setContent(e.target.value)}
           value={content}
         />
+        <div className='grid grid-cols-2 gap-4'>
+          {preview.map(
+            (url, index) =>
+              url && (
+                <div key={index} className='relative'>
+                  <Image
+                    src={url.dataUrl}
+                    alt='preview'
+                    layout='responsive'
+                    width={500}
+                    height={300}
+                    className='rounded-md object-cover'
+                  />
+                  <button
+                    type='button'
+                    className='absolute right-2 top-2 rounded-full bg-black bg-opacity-50 p-1 text-white'
+                    onClick={() =>
+                      setPreview((prev) => {
+                        const next = [...prev];
+                        next[index] = null;
+                        return next;
+                      })
+                    }
+                  >
+                    <XCircle size={24} />
+                  </button>
+                </div>
+              ),
+          )}
+        </div>
         <div className='flex items-center justify-between'>
           <div className='flex items-center gap-4'>
-            <input type='file' name='image' className='hidden' ref={imageRef} />
+            <input
+              type='file'
+              name='image'
+              multiple
+              className='hidden'
+              ref={imageRef}
+              onChange={handleUpload}
+            />
             <ImageSquare
               size={24}
-              className='text-cool-500 hover:text-cool-600 cursor-pointer transition-colors'
+              className='cursor-pointer text-cool-500 transition-colors hover:text-cool-600'
               onClick={() => imageRef.current?.click()}
             />
           </div>
